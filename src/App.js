1d71522643c6749cbb8aaab58fcd16a9c8f58bff
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2021-02-28 20:16:01 -0700

    2.13 AppBar Transitions

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Header } from './components';
 
-import { Container } from '@material-ui/core';
+import { Container, Box } from '@material-ui/core';
 
 function App() {
   return (
     <Container>
       <Header />
-      Hello
+      Hello!
+      {/* <Box my={2}>
+        {[...new Array(240)]
+          .map(
+            () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+          )
+          .join('\n')}
+      </Box> */}
     </Container>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,36 @@
 import React from 'react';
 
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  useScrollTrigger,
+} from '@material-ui/core';
+
+function ElevationScroll(props) {
+  const { children } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
 
 const Header = (props) => {
   return (
-    <AppBar position='fixed'>
-      <Toolbar>
-        <Typography>Arc Development</Typography>{' '}
-      </Toolbar>
-    </AppBar>
+    <ElevationScroll>
+      <AppBar position='fixed'>
+        <Toolbar>
+          <Typography>Arc Development</Typography>
+        </Toolbar>
+      </AppBar>
+    </ElevationScroll>
   );
 };
 
