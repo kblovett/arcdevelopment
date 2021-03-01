00b500c97080b04190ae24afbcf39478c26fb2df
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2021-02-28 22:04:42 -0700

    2.15-16 Themes - Default, Palette

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React from 'react';
 import { Header } from './components';
 
-import { Container } from '@material-ui/core';
+import { Container, Box, Typography } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 
-import { dark, light } from './components/themes';
+import { dark } from './components/themes';
 
 function App() {
   return (
     <ThemeProvider theme={dark}>
       <Container>
         <Header />
-        Hello!
+        <Box my={2}>
+          <Typography>
+            {[...new Array(100)]
+              .map(
+                () =>
+                  `Cras mattis consectetur purus sit amet fermentum. Cras justo
+                odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
+                risus, porta ac consectetur ac, vestibulum at eros. Praesent
+                commodo cursus magna, vel scelerisque nisl consectetur et.`
+              )
+              .join('\n')}
+          </Typography>
+        </Box>
       </Container>
     </ThemeProvider>
   );
 }
 
 export default App;
-
-/* <Box my={2}>
-  {[...new Array(240)]
-    .map(
-      () => `Cras mattis consectetur purus sit amet fermentum.
-      Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-      Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-      Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-    )
-    .join('\n')}
-</Box> */
--- src/components/themes/dark.js
+++ src/components/themes/dark.js
@@ -1,14 +1,30 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { deepOrange, green, blueGrey } from '@material-ui/core/colors';
+
+// const pMain = '#222831';
+// const pLight = '#00adb5';
+// const pDark = '#393e46';
+// const pCText = '#eeeeee';
+
+const arcBlue = '#0B72B9';
+const arcOrange = '#FFBA60';
 
 const dark = createMuiTheme({
   palette: {
     type: 'dark',
-    // primary: blueGrey,
-    // secondary: green,
-  },
-  status: {
-    danger: deepOrange,
+    common: {
+      blue: `${arcBlue}`,
+      orange: `${arcOrange}`,
+    },
+    primary: {
+      main: `${arcBlue}`,
+    },
+    secondary: {
+      main: `${arcOrange}`,
+    },
+    // background: {
+    //   paper: `${pDark}`,
+    //   default: `${pDark}`,
+    // },
   },
 });
 
